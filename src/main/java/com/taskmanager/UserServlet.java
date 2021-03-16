8598f0e8555b609aa84e4748173a558871c8e812
MRFIJ <zqq1111@yandex.ru>
2021-03-16 16:01:19 +0400

    2

--- src/main/java/com/taskmanager/UserServlet.java
+++ src/main/java/com/taskmanager/UserServlet.java
@@ -2,7 +2,7 @@ package com.taskmanager;
 
 import com.taskmanager.entity.User;
 
-import javax.ejb.EJB;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,26 +12,25 @@ import java.io.IOException;
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
-    @EJB
-    userService javaBean;
 
+
+    userService userService;
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    String firstName = req.getParameter("firstName");
-    String secondName = req.getParameter("secondName");
-    String userName = req.getParameter("userName");
-    String password = req.getParameter("password");
-    User user = new User(firstName,secondName,userName,password);
-    javaBean.saveUser(user);
-    resp.getWriter().println("страница такс менеджера");
+        String firstName = req.getParameter("firstName");
+        String secondName = req.getParameter("secondName");
+        String userName = req.getParameter("userName");
+        String password = req.getParameter("password");
+        User user = new User(firstName, secondName, userName, password);
+        userService.saveUser(user);
+        resp.getWriter().println("страница такс менеджера");
     }
 }
